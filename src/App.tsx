4c1cefa720d7d98642f4c5455f091daf1878691d
Jordan Alex <60239297+jorale2415@users.noreply.github.com>
2025-08-23 12:08:57 -0500

    Fix login routing and blank screen issue

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
 import TransferForm from './components/TransferForm';
 import TransactionHistory from './components/TransactionHistory';
+import TransferHistory from './components/TransferHistory';
 import Layout from './components/Layout';
 import ProtectedRoute from './components/ProtectedRoute';
 import NotificationContainer from './components/NotificationContainer';
@@ -18,21 +19,32 @@ export default function App() {
         <NotificationProvider>
           <NotificationContainer />
           <Routes>
+            {/* Public routes */}
             <Route path="/login" element={<LoginForm />} />
             <Route path="/register" element={<RegisterForm />} />
-            <Route path="/" element={
-              <ProtectedRoute>
-                <Layout>
-                  <Routes>
-                    <Route index element={<Navigate to="/dashboard" replace />} />
-                    <Route path="dashboard" element={<Dashboard />} />
-                    <Route path="home" element={<Dashboard />} />
-                    <Route path="transfer" element={<TransferForm />} />
-                    <Route path="history" element={<TransactionHistory />} />
-                  </Routes>
-                </Layout>
-              </ProtectedRoute>
-            } />
+            
+            {/* Protected routes */}
+            <Route path="/" element={<ProtectedRoute />}>
+              <Route path="" element={<Navigate to="/dashboard" replace />} />
+              <Route path="dashboard" element={<Layout />}>
+                <Route index element={<Dashboard />} />
+              </Route>
+              <Route path="home" element={<Layout />}>
+                <Route index element={<Dashboard />} />
+              </Route>
+              <Route path="transfer" element={<Layout />}>
+                <Route index element={<TransferForm />} />
+              </Route>
+              <Route path="history" element={<Layout />}>
+                <Route index element={<TransactionHistory />} />
+              </Route>
+              <Route path="audit" element={<Layout />}>
+                <Route index element={<TransferHistory />} />
+              </Route>
+            </Route>
+            
+            {/* Catch all route */}
+            <Route path="*" element={<Navigate to="/dashboard" replace />} />
           </Routes>
         </NotificationProvider>
       </AuthProvider>
